0b92ccea310597f79d239fbe741031de946dd573
Miriam <57719825+herbstsonne@users.noreply.github.com>
2022-12-18 18:19:15 +0100

    Initial Commit

---
+++ ChatOpenAI/ChatOpenAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatOpenAIApp.swift
+//  ChatOpenAI
+//
+//  Created by Miriam Pfaffenbach on 18.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ChatOpenAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatOpenAIUITests/ChatOpenAIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChatOpenAIUITestsLaunchTests.swift
+//  ChatOpenAIUITests
+//
+//  Created by Miriam Pfaffenbach on 18.12.22.
+//
+
+import XCTest
+
+final class ChatOpenAIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
